ab1b9979579d118f943ae15f46699dd765244706
Kevin-Contreras <65563112+Kevin-Contreras@users.noreply.github.com>
2020-06-06 14:25:26 -0600

    Update CODIGO.java

--- CODIGO.java
+++ CODIGO.java
@@ -2,197 +2,196 @@ package practicanueva;
 import java.util.Scanner;
 public class practicanueva {
 	public static void main(String[] args) {
-		System.out.println("SOPA DE LETRAS");
-		System.out.println("****************************");
-		String menu = "";
-		String palabrasEncontradas="0";
-		String palabrasNoEncontradas ="0";
-		int menuParseado=0;
-		String menuSegundo = "";
-		String puntosfinal="";
-		int menuSegundoparseado=0;
-		String tercerMenu ="";
-		String nombresGuardados ="";
-		String nombresGuardadosGanados ="";
-		String palabrasGuardadas ="";
-		String todo="";
-		String puntosFinal="";
-		String arreglopalabras[]=new String[1];
-		String nombre ="";
-		int contador2=0;
-		int parseado =0;
-		int estadisticasParcear = 0;
-		do{
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("1)nueva partida 2)estadisticas 3)salir del juego");
-		System.out.println("****************************");
-		System.out.println("ingrese el numero de la opcion que desea ingresar");
-		String palabrasNoGuardadas="";
-		menu = scanner.next(); 
-		menuParseado= Integer.parseInt(menu);
-		switch(menuParseado){
-		case 1:
-			System.out.println("¿cual es tu nombre?");
-			 nombre = scanner.next();
-			System.out.println("Escoje el tamano del la matriz");
-			int par =0;
-			String tamañoMatriz="";
-			while(par<10){
-				 tamañoMatriz  = scanner.next();
-				 par = Integer.parseInt(tamañoMatriz);
-				 if(par<10){
-					 System.out.println("tiene que ser igual o mayor a 10 el arreglo");	 
-				 }else{
-				 }
+System.out.println("SOPA DE LETRAS");
+System.out.println("****************************");
+String menu = "";
+String palabrasEncontradas="0";
+String palabrasNoEncontradas ="0";
+int menuParseado=0;
+String menuSegundo = "";
+String puntosfinal="";
+int menuSegundoparseado=0;
+String tercerMenu ="";
+String nombresGuardados ="";
+String nombresGuardadosGanados ="";
+String palabrasGuardadas ="";
+String todo="";
+String puntosFinal="";
+String arreglopalabras[]=new String[1];
+String nombre ="";
+int contador2=0;
+int parseado =0;
+int estadisticasParcear = 0;
+do{
+Scanner scanner = new Scanner(System.in);
+System.out.println("1)nueva partida 2)estadisticas 3)salir del juego");
+System.out.println("****************************");
+System.out.println("ingrese el numero de la opcion que desea ingresar");
+String palabrasNoGuardadas="";
+menu = scanner.next(); 
+menuParseado= Integer.parseInt(menu);
+switch(menuParseado){
+case 1:
+	System.out.println("Â¿cual es tu nombre?");
+	 nombre = scanner.next();
+	System.out.println("Escoje el tamano del la matriz");
+	int par =0;
+	String tamaÃ±oMatriz="";
+	while(par<10){ tamaÃ±oMatriz  = scanner.next();
+par = Integer.parseInt(tamaÃ±oMatriz);
+if(par<10){
+	System.out.println("tiene que ser igual o mayor a 10 el arreglo");	 
+		}else{
+		}
 				 
-			}
+}
 			
-			if(par<9){
-				System.out.println("tiene que ser igual o mayor a 10 el arreglo");
-			}else{
-				do{
-					System.out.println("****************************");
-					System.out.println("1)menu palabras 2)jugar 3)salir de la partida");
-					System.out.println("****************************");
+if(par<9){
+	System.out.println("tiene que ser igual o mayor a 10 el arreglo");
+}else{
+	do{
+		System.out.println("****************************");
+		System.out.println("1)menu palabras 2)jugar 3)salir de la partida");
+		System.out.println("****************************");
+			System.out.println("ingrese el numero de la opcion que desea ingresar");
+				menuSegundo  = scanner.next();
+		menuSegundoparseado = Integer.parseInt(menuSegundo);
+				switch(menuSegundoparseado){
+				case 1:
+					do{System.out.println("****************************");
+					 System.out.println("1)insertar 2)insertar todas 3)modificar 4)eliminar 5)salir");
+					 System.out.println("****************************");
 						System.out.println("ingrese el numero de la opcion que desea ingresar");
-						 menuSegundo  = scanner.next();
-						 menuSegundoparseado = Integer.parseInt(menuSegundo);
-						 switch(menuSegundoparseado){
-						 case 1:
-							 do{System.out.println("****************************");
-								 System.out.println("1)insertar 2)insertar todas 3)modificar 4)eliminar 5)salir");
-								 System.out.println("****************************");
-								 System.out.println("ingrese el numero de la opcion que desea ingresar");
-								 tercerMenu  = scanner.next();
-								 parseado = Integer.parseInt(tercerMenu);
-								 switch(parseado){
-								 case 1:
-								 System.out.println("ingrese el numero de palabras");
-								 int numeropalabras  = scanner.nextInt();
-								 System.out.println("ingrese las palabras");
-								 int a = 0;
+						tercerMenu  = scanner.next();
+						parseado = Integer.parseInt(tercerMenu);
+						switch(parseado){
+					 case 1:
+						System.out.println("ingrese el numero de palabras");
+						int numeropalabras  = scanner.nextInt();
+						System.out.println("ingrese las palabras");
+						int a = 0;
 								 
-									 while(a<numeropalabras){
+						 while(a<numeropalabras){
 										 
-										 String palabras  = scanner.next();
-										 if(palabras.length()>=5&&palabras.length()<=10){
-										 if(palabras.length()<par){
+							 String palabras  = scanner.next();
+								if(palabras.length()>=5&&palabras.length()<=10){
+								if(palabras.length()<par){
 											 
-												 palabrasNoGuardadas+=palabras+"$";
-												 palabrasGuardadas+=palabrasNoGuardadas;
+										palabrasNoGuardadas+=palabras+"$";
+										palabrasGuardadas+=palabrasNoGuardadas;
 																					 
-												 ++contador2;
-												 ++a;	 
-											 }else{
-												 System.out.println("la palabra tiene que ser menor a la matriz");
+									 ++contador2;
+									 ++a;	 
+									}else{
+										System.out.println("la palabra tiene que ser menor a la matriz");
 												
-											 }
-												 
-										 }else{
-											 System.out.println("la palabra tiene que tener entre 5 y 10 caracteres");
-										 }
-										
-									 }
-									 break;
-								 case 2 :
-									 System.out.println("ingrese las palabras con un espacio de $ por cada palabra");
-									 palabrasNoGuardadas += scanner.next();
-									 palabrasGuardadas += palabrasNoGuardadas;
-									 break;
-								 case 3:
-									 if(palabrasNoGuardadas==""){ 
-									 }else{
-										 System.out.println("escriba la palabra que quiere modificar");
-										 String modificar = scanner.next()+"$"; 
-										 if(palabrasNoGuardadas.contains(modificar)==true){
-											 System.out.println("escriba la palabra nueva");
-											 String palabraNueva = scanner.next()+"$"; 
-											palabrasNoGuardadas= palabrasNoGuardadas.replace(modificar, palabraNueva);
-											palabrasGuardadas = palabrasNoGuardadas;
-											 System.out.println("se ha modificado");
-											 System.out.println(palabrasGuardadas);
-										 }else{
-											 System.out.println("no ha sido encontrada");
-										 }
-									 }
-									 break;
-								 case 4 : 
-									 if(palabrasGuardadas==""){	 
-									 }
-									 else{
-										 System.out.println("escriba la palabra que quiere eliminar");
-										 String eliminar = scanner.next()+"$";
-										 if(palabrasNoGuardadas.contains(eliminar)==true){
-											 palabrasNoGuardadas = palabrasNoGuardadas.replace(eliminar, "");
-											 palabrasGuardadas = palabrasNoGuardadas; 
-												System.out.println(palabrasGuardadas);
-										 }else{
-										 System.out.println("la palabra no fue encontrada");
-										 }
-									 }
-									 break;
 								 }
-							 }while(parseado != 5);
-							 break;
-						 case 2 :
-							 if(palabrasNoGuardadas==""){
-								 System.out.println("ho hay palabras agregadas");
-							 }else{
-								 int tamañoMatrizparseada = Integer.parseInt(tamañoMatriz);
-									String matrizPrincipal[][]= new String [tamañoMatrizparseada][tamañoMatrizparseada];
-									String abc[] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"};
-									for(int c =0; c<matrizPrincipal.length;++c){
-										for(int d =0; d<matrizPrincipal.length;++d){
-											 double random =  Math.random()*26; 
-											 int random2 = (int)random;
-											matrizPrincipal[c][d]= abc[random2];
-										}
-									}
-									String [] palabrasSeparadas = palabrasNoGuardadas.split("\\$");
-									String concatenarNumero="";
-									String concatenarNumero2="";
-									int palabrasNoEncontradas1 =0;
-									for(String elemento: palabrasSeparadas){
-										 double random3 =  Math.random()*tamañoMatrizparseada; 
-										 int random4 = (int)random3;
-										 double random10 =  Math.random()*4; 
-										 int random11 = (int)random10;
-										 double verdadero =  Math.random()*2; 
-										 int verdadero1 = (int)verdadero;
-										  concatenarNumero += String.valueOf(random11)+"$";
-										  concatenarNumero2+=String.valueOf(random4)+"$";
+												 
+								}else{
+									System.out.println("la palabra tiene que tener entre 5 y 10 caracteres");
+								}
+										
+							}
+						 break;
+						case 2 :
+						 System.out.println("ingrese las palabras con un espacio de $ por cada palabra");
+						 palabrasNoGuardadas += scanner.next();
+							palabrasGuardadas += palabrasNoGuardadas;
+							break;
+						case 3:
+							if(palabrasNoGuardadas==""){ 
+							}else{
+							 System.out.println("escriba la palabra que quiere modificar");
+								String modificar = scanner.next()+"$"; 
+								if(palabrasNoGuardadas.contains(modificar)==true){
+								 System.out.println("escriba la palabra nueva");
+								 String palabraNueva = scanner.next()+"$"; 
+								palabrasNoGuardadas= palabrasNoGuardadas.replace(modificar, palabraNueva);
+								palabrasGuardadas = palabrasNoGuardadas;
+									System.out.println("se ha modificado");
+									System.out.println(palabrasGuardadas);
+								}else{
+									System.out.println("no ha sido encontrada");
+							 }
+							}
+							break;
+					 case 4 : 
+							if(palabrasGuardadas==""){	 
+							}
+						 else{
+							 System.out.println("escriba la palabra que quiere eliminar");
+								String eliminar = scanner.next()+"$";
+								if(palabrasNoGuardadas.contains(eliminar)==true){
+									palabrasNoGuardadas = palabrasNoGuardadas.replace(eliminar, "");
+									palabrasGuardadas = palabrasNoGuardadas; 
+									System.out.println(palabrasGuardadas);
+								}else{
+								System.out.println("la palabra no fue encontrada");
+								}
+							}
+							break;
+					 }
+					}while(parseado != 5);
+				 break;
+				case 2 :
+					if(palabrasNoGuardadas==""){
+					 System.out.println("ho hay palabras agregadas");
+					}else{
+						int tamaÃ±oMatrizparseada = Integer.parseInt(tamaÃ±oMatriz);
+						String matrizPrincipal[][]= new String [tamaÃ±oMatrizparseada][tamaÃ±oMatrizparseada];
+						String abc[] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","Ã±","o","p","q","r","s","t","u","v","w","x","y","z"};
+						for(int c =0; c<matrizPrincipal.length;++c){
+							for(int d =0; d<matrizPrincipal.length;++d){
+								 double random =  Math.random()*26; 
+									int random2 = (int)random;
+								matrizPrincipal[c][d]= abc[random2];
+							}
+						}
+						String [] palabrasSeparadas = palabrasNoGuardadas.split("\\$");
+						String concatenarNumero="";
+						String concatenarNumero2="";
+						int palabrasNoEncontradas1 =0;
+						for(String elemento: palabrasSeparadas){
+								double random3 =  Math.random()*tamaÃ±oMatrizparseada; 
+								int random4 = (int)random3;
+								double random10 =  Math.random()*4; 
+								int random11 = (int)random10;
+								double verdadero =  Math.random()*2; 
+								int verdadero1 = (int)verdadero;
+									concatenarNumero += String.valueOf(random11)+"$";
+									concatenarNumero2+=String.valueOf(random4)+"$";
 										  
-										 if(verdadero1==1){
-											 for(int k =0;k<elemento.length();++k){	 
-												 double random5 =  Math.random()*2; 
-												 int random6 = (int)random5;
-												 char letra = elemento.charAt(k);
-												 String letrastring = Character.toString(letra);
-												 matrizPrincipal[random4][random11+k]= letrastring;		 
-									 }
-										 }else{
-											 for(int U =0;U<elemento.length();++U){	 
-												 double random5 =  Math.random()*2; 
-												 int random9 = (int)random5;
-												 char letra = elemento.charAt(U);
-												 String letrastrings = Character.toString(letra);
-												 matrizPrincipal[U+random11][random4]= letrastrings;		 
-									 }
-										 }			 
-									};
-									for(int l =0; l<matrizPrincipal.length;++l){
-										for(int m =0; m<matrizPrincipal.length;++m){
-											System.out.print("|"+matrizPrincipal[m][l]+"|");
-										}
-										System.out.println();
-									}
-									System.out.println("coloque las palabras encontradas");
-									int vida=3;
-									int putaso2=0;
+							 if(verdadero1==1){
+									for(int k =0;k<elemento.length();++k){	 
+									 double random5 =  Math.random()*2; 
+										int random6 = (int)random5;
+										char letra = elemento.charAt(k);
+										String letrastring = Character.toString(letra);
+										matrizPrincipal[random4][random11+k]= letrastring;		 
+							}
+								}else{
+									for(int U =0;U<elemento.length();++U){	 
+									 double random5 =  Math.random()*2; 
+										int random9 = (int)random5;
+										char letra = elemento.charAt(U);
+										String letrastrings = Character.toString(letra);
+										matrizPrincipal[U+random11][random4]= letrastrings;		 
+						 }
+								}			 
+						};
+						for(int l =0; l<matrizPrincipal.length;++l){
+							for(int m =0; m<matrizPrincipal.length;++m){
+								System.out.print("|"+matrizPrincipal[m][l]+"|");
+							}
+							System.out.println();
+						}
+						System.out.println("coloque las palabras encontradas");
+						int vida=3;
+						int putaso2=0;
 									int puntos =20;
 									int cambiarVida = 0;
 									int aciertos=0;
-									int l =0;
+						int l =0;
 									
 									for(String hoja : palabrasSeparadas){
 										cambiarVida+=1;
